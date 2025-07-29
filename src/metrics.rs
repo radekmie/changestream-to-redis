ba99b2d4ba84f1dad8a336c20f9966e578960708
Radosław Miernik <radekmie@gmail.com>
2025-07-29 11:34:00 +0200

    Standardized Prometheus metrics.

--- src/metrics.rs
+++ src/metrics.rs
@@ -8,21 +8,21 @@ use tokio::{net::TcpListener, spawn};
 
 pub static LAST_EVENT_GAUGE: LazyLock<IntGauge> = LazyLock::new(|| {
     register_int_gauge!(
-        "changestream_to_redis_last_mongo_event",
+        "changestream_to_redis_last_mongo_event_timestamp_seconds",
         "Timestamp of last MongoDB event"
     )
     .unwrap()
 });
 pub static MONGO_COUNTER: LazyLock<IntCounter> = LazyLock::new(|| {
     register_int_counter!(
-        "changestream_to_redis_mongo_events",
+        "changestream_to_redis_mongo_events_total",
         "Number of MongoDB events"
     )
     .unwrap()
 });
 pub static REDIS_COUNTER: LazyLock<IntCounter> = LazyLock::new(|| {
     register_int_counter!(
-        "changestream_to_redis_redis_events",
+        "changestream_to_redis_redis_events_total",
         "Number of Redis invokes"
     )
     .unwrap()
@@ -44,5 +44,8 @@ pub async fn serve(address: String) {
 async fn server_metrics(_: Request<Incoming>) -> Result<Response<String>, Infallible> {
     let mut buffer = Vec::new();
     TextEncoder::new().encode(&gather(), &mut buffer).unwrap();
-    Ok(Response::new(String::from_utf8(buffer).unwrap()))
+    Ok(Response::builder()
+        .header("Content-Type", "text/plain")
+        .body(String::from_utf8(buffer).unwrap())
+        .unwrap())
 }
